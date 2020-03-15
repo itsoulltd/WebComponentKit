f0cbc41226947658e19d621af7de5ac81433be4b
Towhid Islam <m.towhid.islam@gmail.com>
2020-03-16 01:44:32 +0600

    updated

--- HttpRestClient/src/main/java/com/infoworks/lab/rest/models/events/Event.java
+++ HttpRestClient/src/main/java/com/infoworks/lab/rest/models/events/Event.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.Map;
+import java.util.Objects;
 
 public class Event extends Entity implements Externalizable {
 
@@ -50,5 +51,19 @@ public class Event extends Entity implements Externalizable {
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
         Map<String, Object> data = (Map<String, Object>) in.readObject();
         unmarshallingFromMap(data, true);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(uuid, event.uuid) &&
+                Objects.equals(timestamp, event.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, timestamp);
     }
 }

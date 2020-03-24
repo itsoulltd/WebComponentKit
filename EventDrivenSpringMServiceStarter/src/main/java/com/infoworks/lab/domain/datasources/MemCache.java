8afc35b13374f177da6e1041f818ec24ebebc743
Towhid Islam <m.towhid.islam@gmail.com>
2020-03-24 07:50:25 +0600

    RedissonClient bean added and implementation of MemCache.java <DataSource>

---
+++ EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/domain/datasources/MemCache.java
@@ -0,0 +1,90 @@
+package com.infoworks.lab.domain.datasources;
+
+import com.it.soul.lab.data.base.DataSource;
+import com.it.soul.lab.sql.entity.EntityInterface;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MemCache<Entity extends EntityInterface> implements DataSource<String, Entity> {
+
+    private Logger LOG = Logger.getLogger(this.getClass().getSimpleName());
+    private RedissonClient client;
+    private static final String CLASS_NAME_KEY = "classname";
+
+    public MemCache(RedissonClient client) {
+        this.client = client;
+    }
+
+    public Entity read(String key){
+        RMap rData = client.getMap(key);
+        if (rData != null && rData.size() > 0){
+            Map<String, Object> data = new HashMap<>(rData.size());
+            Iterator<Map.Entry<String, Object>> itr = rData.entrySet().iterator();
+            while (itr.hasNext()){
+                Map.Entry<String, Object> entry = itr.next();
+                data.put(entry.getKey(), entry.getValue());
+            }
+            try {
+                //Retrieving: Type
+                Entity instance = initFromClassname(data.get(CLASS_NAME_KEY));
+                instance.unmarshallingFromMap(data, true);
+                return instance;
+            } catch (InstantiationException e) {
+                LOG.log(Level.WARNING, e.getMessage(), e);
+            } catch (IllegalAccessException e) {
+                LOG.log(Level.WARNING, e.getMessage(), e);
+            } catch (ClassNotFoundException e) {
+                LOG.log(Level.WARNING, e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+
+    private Entity initFromClassname(Object classname)
+            throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        //
+        if (classname == null) return null;
+        String classFullName = classname.toString();
+        if (classFullName.isEmpty()) return null;
+        //
+        Entity instance = (Entity) Class.forName(classFullName).newInstance();
+        return instance;
+    }
+
+    @Override
+    public Entity remove(String key) {
+        Entity value = read(key);
+        //then clear the cache:
+        RMap rData = client.getMap(key);
+        if (rData.size() > 0){
+            rData.clear();
+        }
+        return value;
+    }
+
+    @Override
+    public void put(String key, Entity entity) {
+        Map<String, Object> data = entity.marshallingToMap(true);
+        RMap rData = client.getMap(key);
+        if (rData.size() > 0){
+            rData.clear();
+        }
+        data.entrySet().stream()
+                .filter(entry -> entry.getValue() != null)
+                .forEach(entry -> rData.put(entry.getKey(), entry.getValue()));
+        //Saving: Type
+        String classFullName = entity.getClass().getName();
+        rData.put(CLASS_NAME_KEY, classFullName);
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        return client.getMap(key).size() > 0;
+    }
+}
--- EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/webapp/config/BeanConfig.java
+++ EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/webapp/config/BeanConfig.java
@@ -1,8 +1,13 @@
 package com.infoworks.lab.webapp.config;
 
+import com.infoworks.lab.domain.datasources.MemCache;
+import com.infoworks.lab.domain.entities.Passenger;
 import com.infoworks.lab.jsql.ExecutorType;
 import com.infoworks.lab.jsql.JsqlConfig;
 import com.it.soul.lab.sql.SQLExecutor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
@@ -38,6 +43,26 @@ public class BeanConfig {
     @Bean("AppDBNameKey")
     String appDBNameKey(){
         return env.getProperty("app.db.name");
+    }
+
+    @Bean
+    RedissonClient getRedisClient(){
+        String redisHost = env.getProperty("app.redis.host") != null
+                ? env.getProperty("app.redis.host") : "localhost";
+        String redisPort = env.getProperty("app.redis.port") != null
+                ? env.getProperty("app.redis.port") : "6379";
+        Config conf = new Config();
+        conf.useSingleServer()
+                .setAddress(String.format("redis://%s:%s",redisHost, redisPort))
+                .setRetryAttempts(5)
+                .setRetryInterval(1500);
+        //RedissionClient instance are fully-thread safe.
+        return Redisson.create(conf);
+    }
+
+    @Bean("passengerCache")
+    MemCache<Passenger> getPassengerCache(){
+        return new MemCache<>(getRedisClient());
     }
 
 }

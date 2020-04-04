00713e0b32431b3ea43a022484c7885e1e8f7df7
Towhid Islam <m.towhid.islam@gmail.com>
2020-04-04 14:15:19 +0600

    AbstractTask constructor get refactored

--- EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/domain/beans/tasks/base/AbstractTask.java
+++ EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/domain/beans/tasks/base/AbstractTask.java
@@ -2,7 +2,11 @@ package com.infoworks.lab.domain.beans.tasks.base;
 
 import com.infoworks.lab.beans.tasks.definition.Task;
 import com.infoworks.lab.rest.models.Message;
+import com.it.soul.lab.sql.query.models.Property;
+import com.it.soul.lab.sql.query.models.Row;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.function.Function;
 
 public abstract class AbstractTask implements Task {
@@ -11,14 +15,41 @@ public abstract class AbstractTask implements Task {
     private Message message;
     private Function<Message, Message> converter;
 
-    public AbstractTask() {}
+    public AbstractTask() {
+        this.message = new Message();
+    }
 
     public AbstractTask(String message) {
-        this.message = new Message();
+        this();
         this.message.setPayload(message);
     }
 
     public AbstractTask(String message, Function<Message, Message> converter) {
+        this(message);
+        this.converter = converter;
+    }
+
+    public AbstractTask(Property...properties){
+        this();
+        Row row = new Row();
+        row.setProperties(Arrays.asList(properties));
+        try {
+            this.message.setPayload(Message.marshal(row.keyObjectMap()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public AbstractTask(Property[] properties, Function<Message, Message> converter){
+        this(properties);
+        this.converter = converter;
+    }
+
+    public AbstractTask(Message message) {
+        this.message = message;
+    }
+
+    public AbstractTask(Message message, Function<Message, Message> converter) {
         this(message);
         this.converter = converter;
     }
@@ -45,5 +76,9 @@ public abstract class AbstractTask implements Task {
     @Override
     public Function<Message, Message> getConverter() {
         return converter;
+    }
+
+    public void setConverter(Function<Message, Message> converter) {
+        this.converter = converter;
     }
 }

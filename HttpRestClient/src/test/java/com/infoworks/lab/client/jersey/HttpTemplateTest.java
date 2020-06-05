726b52cf5256ac68c64558a2dda8ee63083ce921
mtowhidislam <m.towhid.islam@gmail.com>
2020-06-05 18:44:35 +0600

    updated

--- HttpRestClient/src/test/java/com/infoworks/lab/client/jersey/HttpTemplateTest.java
+++ HttpRestClient/src/test/java/com/infoworks/lab/client/jersey/HttpTemplateTest.java
@@ -5,7 +5,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class HttpTemplateTest {
@@ -45,11 +49,95 @@ public class HttpTemplateTest {
 
     @Test
     public void jerseyTest(){
+        jersey("/path-a", "path-b", new QueryParam("status",null), new QueryParam("offset","0"), new QueryParam("limit", "10"));
+    }
 
+    public <T> void jersey(T...params){
+        //Means T...params are arbitrary value e.g. "/path-a", "path-b", QueryParam("offset","0"), QueryParam("limit","10") ... etc
+        //First: Separate Paths from mixed array:
+        String[] paths = Arrays.stream(params).filter(obj -> obj instanceof String).collect(Collectors.toList()).toArray(new String[0]);
+        //Then: Separate QueryParam from mixed array:
+        QueryParam[] queryParams = Arrays.stream(params).filter(obj -> obj instanceof QueryParam).collect(Collectors.toList()).toArray(new QueryParam[0]);
+        //Finally
+        List<String> collector = new ArrayList<>(Arrays.asList(paths));
+        collector.add(urlencodedQueryParam(queryParams));
+        System.out.println(validatePaths(collector.toArray(new String[0])));
     }
 
     @Test
-    public void httookTests(){
-
+    public void okhttpTest(){
+        okhttp("/path-a", "path-b", new QueryParam("status",null), new QueryParam("offset","0"), new QueryParam("limit", "10"));
     }
+
+
+    public <T> void okhttp(T...params){
+        //Means T...params are arbitrary value e.g. "/path-a", "path-b", QueryParam("offset","0"), QueryParam("limit","10") ... etc
+        //First: Separate Paths from mixed array:
+        List<String> paths = new ArrayList<>();
+        for (Object query : params) {
+            if (query instanceof String)
+                paths.add((String) query);
+        }
+        List<String> collector = new ArrayList<>(paths);
+        //Then: Separate QueryParam from mixed array:
+        List<QueryParam> queryParams = new ArrayList<>();
+        for (Object query : params) {
+            if (query instanceof QueryParam)
+                queryParams.add((QueryParam) query);
+        }
+        String queryParam = urlencodedQueryParam(queryParams.toArray(new QueryParam[0]));
+        collector.add(queryParam);
+        //Finally:
+        System.out.println(validatePaths(collector.toArray(new String[0])));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Copied from Main Source-Block:
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private String urlencodedQueryParam(QueryParam...params){
+        if (params == null) return "";
+        StringBuffer buffer = new StringBuffer();
+        //Separate Paths:
+        List<String> pathsBag = new ArrayList<>();
+        for (QueryParam query : params) {
+            if (query.getValue() != null && !query.getValue().isEmpty()) {
+                continue;
+            }
+            pathsBag.add(query.getKey());
+        }
+        buffer.append(validatePaths(pathsBag.toArray(new String[0])));
+        //Incorporate QueryParams:
+        buffer.append("?");
+        for (QueryParam query : params){
+            if (query.getValue() == null || query.getValue().isEmpty()){
+                continue;
+            }
+            try {
+                buffer.append(query.getKey()
+                        + "="
+                        + URLEncoder.encode(query.getValue(), "UTF-8")
+                        + "&");
+            } catch (UnsupportedEncodingException e) {}
+        }
+        String value = buffer.toString();
+        value = value.substring(0, value.length()-1);
+        return value;
+    }
+
+    private StringBuffer validatePaths(String... params) {
+        StringBuffer buffer = new StringBuffer();
+        for (String str : params) {
+            String trimmed = str.trim();
+            if (trimmed.length() > 2 && trimmed.endsWith("/")) trimmed = trimmed.substring(0, trimmed.length() - 1);
+            if(trimmed.startsWith("/"))
+                buffer.append(trimmed);
+            else if(trimmed.startsWith("?"))
+                buffer.append(trimmed);
+            else
+                buffer.append("/" + trimmed);
+        }
+        return buffer;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }

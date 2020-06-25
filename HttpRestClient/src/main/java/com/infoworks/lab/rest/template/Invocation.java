2156562d4884ee90b3eac0ad7e19230c618242c6
mtowhidislam <m.towhid.islam@gmail.com>
2020-06-25 20:01:13 +0600

    adding doc

--- HttpRestClient/src/main/java/com/infoworks/lab/rest/template/Invocation.java
+++ HttpRestClient/src/main/java/com/infoworks/lab/rest/template/Invocation.java
@@ -5,20 +5,32 @@ import com.it.soul.lab.sql.entity.EntityInterface;
 import com.it.soul.lab.sql.query.models.Property;
 
 import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 public interface Invocation<R extends AutoCloseable, M extends Object> {
 
+    /**
+     * In Jersey 2:
+     * Connect timeout interval, in milliseconds. The value MUST be an instance convertible to Integer.
+     * A value of zero (0) is equivalent to an interval of infinity.
+     * The default value is infinity (0).
+     *
+     * In OkHttp 3:
+     * We can specify as we wish.
+     */
     enum TIMEOUT{
-        CONNECT("CONNECT_TIMEOUT", 10000),
-        READ("READ_TIMEOUT", 10000),
-        WRITE("WRITE_TIMEOUT", 10000);
+        CONNECT("CONNECT_TIMEOUT", 10000, TimeUnit.MILLISECONDS),
+        READ("READ_TIMEOUT", 10000, TimeUnit.MILLISECONDS),
+        WRITE("WRITE_TIMEOUT", 10000, TimeUnit.MILLISECONDS);
 
         private String key;
         private long value;
+        private TimeUnit unit;
 
-        TIMEOUT(String key, long value) {
+        TIMEOUT(String key, long value, TimeUnit unit) {
             this.key = key;
             this.value = value;
+            this.unit = unit;
         }
 
         public String key(){
@@ -28,7 +40,12 @@ public interface Invocation<R extends AutoCloseable, M extends Object> {
         public long value(){
             return this.value;
         }
+
+        public TimeUnit unit(){
+            return this.unit;
+        }
     }
+
     enum Method {
         GET,
         POST,

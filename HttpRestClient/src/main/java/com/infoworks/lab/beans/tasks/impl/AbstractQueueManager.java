35c08f45f1a3c33a303b4a81ccd3653828dba142
mtowhidislam <m.towhid.islam@gmail.com>
2020-07-07 13:36:49 +0600

    refactoring ...

---
+++ HttpRestClient/src/main/java/com/infoworks/lab/beans/tasks/impl/AbstractQueueManager.java
@@ -0,0 +1,54 @@
+package com.infoworks.lab.beans.tasks.impl;
+
+import com.infoworks.lab.beans.tasks.definition.QueuedTaskLifecycleListener;
+import com.infoworks.lab.beans.tasks.definition.Task;
+import com.infoworks.lab.beans.tasks.definition.TaskManager;
+import com.infoworks.lab.rest.models.Message;
+
+public abstract class AbstractQueueManager implements TaskManager {
+
+    public abstract QueuedTaskLifecycleListener getListener();
+    public abstract void setListener(QueuedTaskLifecycleListener listener);
+
+    @Override
+    public void start(Task task, Message message) {
+        if (task != null){
+            if (getListener() != null)
+                getListener().before(task, State.Forward);
+            //Call Execute:
+            boolean mustAbort = false;
+            Message msg = null;
+            try {
+                msg = task.execute(message);
+            } catch (RuntimeException e) {
+                mustAbort = true;
+                msg = new Message();
+                msg.setPayload(String.format("{\"error\":\"%s\", \"status\":500}", e.getMessage()));
+            }
+            //End Execute:
+            if (getListener() != null) {
+                if (mustAbort) {
+                    getListener().abort(task, msg);
+                } else {
+                    getListener().after(task, State.Forward);
+                    getListener().finished(msg);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void stop(Task task, Message reason) {
+        if (task != null){
+            if (getListener() != null)
+                getListener().before(task, State.Backward);
+            //Call Execute:
+            Message msg = task.abort(reason);
+            //End Execute:
+            if (getListener() != null) {
+                getListener().after(task, State.Backward);
+                getListener().failed(msg);
+            }
+        }
+    }
+}
--- HttpRestClient/src/main/java/com/infoworks/lab/beans/tasks/impl/SynchQueueManager.java
+++ HttpRestClient/src/main/java/com/infoworks/lab/beans/tasks/impl/SynchQueueManager.java
@@ -2,7 +2,6 @@ package com.infoworks.lab.beans.tasks.impl;
 
 import com.infoworks.lab.beans.tasks.definition.QueuedTaskLifecycleListener;
 import com.infoworks.lab.beans.tasks.definition.Task;
-import com.infoworks.lab.beans.tasks.definition.TaskManager;
 import com.infoworks.lab.rest.models.Message;
 
 import java.util.concurrent.ExecutorService;
@@ -10,7 +9,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-public class SynchQueueManager implements TaskManager {
+public class SynchQueueManager extends AbstractQueueManager {
 
     private final Logger logger = Logger.getLogger(this.getClass().getSimpleName());
     private QueuedTaskLifecycleListener listener;
@@ -23,52 +22,12 @@ public class SynchQueueManager implements TaskManager {
 
     @Override
     public void start(Task task, Message message) {
-        getService().submit(() -> startTask(task, message));
-    }
-
-    public void startTask(Task task, Message message) {
-        if (task != null){
-            if (getListener() != null)
-                getListener().before(task, State.Forward);
-            //Call Execute:
-            boolean mustAbort = false;
-            Message msg = null;
-            try {
-                msg = task.execute(message);
-            } catch (RuntimeException e) {
-                mustAbort = true;
-                msg = new Message();
-                msg.setPayload(String.format("{\"error\":\"%s\", \"status\":500}", e.getMessage()));
-            }
-            //End Execute:
-            if (getListener() != null) {
-                if (mustAbort) {
-                    getListener().abort(task, msg);
-                } else {
-                    getListener().after(task, State.Forward);
-                    getListener().finished(msg);
-                }
-            }
-        }
+        getService().submit(() -> super.start(task, message));
     }
 
     @Override
     public void stop(Task task, Message reason) {
-        getService().submit(() -> stopTask(task, reason));
-    }
-
-    public void stopTask(Task task, Message reason) {
-        if (task != null){
-            if (getListener() != null)
-                getListener().before(task, State.Backward);
-            //Call Execute:
-            Message msg = task.abort(reason);
-            //End Execute:
-            if (getListener() != null) {
-                getListener().after(task, State.Backward);
-                getListener().failed(msg);
-            }
-        }
+        getService().submit(() -> super.stop(task, reason));
     }
 
     public QueuedTaskLifecycleListener getListener() {

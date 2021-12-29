e8868101aa8e0b34e1e6a530782d49385c0622dd
Towhid Islam <m.towhid.islam@gmail.com>
2021-12-29 17:38:19 +0600

    Continue..Testing

--- HttpRestClient/src/main/java/com/infoworks/lab/rest/repository/CqlRepository.java
+++ HttpRestClient/src/main/java/com/infoworks/lab/rest/repository/CqlRepository.java
@@ -8,13 +8,11 @@ import com.it.soul.lab.cql.query.CQLSelectQuery;
 import com.it.soul.lab.sql.entity.Entity;
 import com.it.soul.lab.sql.query.QueryType;
 import com.it.soul.lab.sql.query.SQLScalarQuery;
-import com.it.soul.lab.sql.query.models.Where;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
-public interface CqlRepository<E extends Entity, ID> extends RestRepository<E, ID> {
+public interface CqlRepository<E extends Entity, ID> extends iGenericRepository<E, ID> {
 
     CQLExecutor getExecutor();
 
@@ -32,10 +30,8 @@ public interface CqlRepository<E extends Entity, ID> extends RestRepository<E, I
                 query = queryBuilder.columns().from(getEntityType()).where(searchQuery.getPredicate()).build();
             }
             List<E> items = getExecutor().executeSelect(query, getEntityType());
-            int fromIdx = (searchQuery.getPage() - 1) * searchQuery.getSize();
-            if (fromIdx < 0) fromIdx = 0;
-            int toIdx = fromIdx + searchQuery.getSize();
-            if (items.size() < toIdx) toIdx = items.size();
+            int fromIdx = fromIdx(searchQuery);
+            int toIdx = toIdx(searchQuery, fromIdx, items.size());
             List<E> res = items.subList(fromIdx, toIdx);
             return res;
         } catch (SQLException e) {
@@ -77,26 +73,6 @@ public interface CqlRepository<E extends Entity, ID> extends RestRepository<E, I
     }
 
     /**
-     * @param key
-     * @return
-     */
-    default E read(ID key) {
-        List<E> res = null;
-        try {
-            res = Entity.read(getEntityType(), getExecutor(), new Where(getPrimaryKeyName()).isEqualTo(key));
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage());
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e.getMessage());
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e.getMessage());
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return res != null && res.size() > 0 ? res.get(0) : null;
-    }
-
-    /**
      * @return
      * @throws RuntimeException
      */
@@ -113,59 +89,4 @@ public interface CqlRepository<E extends Entity, ID> extends RestRepository<E, I
         }
     }
 
-    /**
-     * @param entity
-     * @return
-     * @throws RuntimeException
-     */
-    @Override
-    default E insert(E entity) throws RuntimeException {
-        if (entity == null) return null;
-        try {
-            entity.insert(getExecutor());
-        } catch (SQLException err) {
-            throw new RuntimeException(err.getMessage());
-        }
-        return null;
-    }
-
-    /**
-     * @param entity
-     * @param id
-     * @return
-     * @throws RuntimeException
-     */
-    @Override
-    default E update(E entity, ID id) throws RuntimeException {
-        E existing = read(id);
-        if (existing != null && entity != null) {
-            Map<String, Object> eData = entity.marshallingToMap(true);
-            eData.remove(getPrimaryKeyName());
-            existing.unmarshallingFromMap(eData, true);
-            try {
-                existing.update(getExecutor());
-            } catch (SQLException err) {
-                throw new RuntimeException(err.getMessage());
-            }
-        }
-        return existing;
-    }
-
-    /**
-     * @param id
-     * @return
-     * @throws RuntimeException
-     */
-    @Override
-    default boolean delete(ID id) throws RuntimeException {
-        E existing = read(id);
-        if (existing != null) {
-            try {
-                return existing.delete(getExecutor());
-            } catch (SQLException e) {
-                throw new RuntimeException(e.getMessage());
-            }
-        }
-        return false;
-    }
 }

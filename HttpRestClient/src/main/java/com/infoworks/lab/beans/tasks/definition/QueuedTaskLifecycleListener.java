491cafcdd83ac8399448a18026430c583decfb3c
Towhid Islam <m.towhid.islam@gmail.com>
2022-10-09 11:20:00 +0600

    task:refactoring: removing default up-stream methods.

--- HttpRestClient/src/main/java/com/infoworks/lab/beans/tasks/definition/QueuedTaskLifecycleListener.java
+++ HttpRestClient/src/main/java/com/infoworks/lab/beans/tasks/definition/QueuedTaskLifecycleListener.java
@@ -3,12 +3,5 @@ package com.infoworks.lab.beans.tasks.definition;
 import com.infoworks.lab.rest.models.Message;
 
 public interface QueuedTaskLifecycleListener extends TaskLifecycleListener {
-
     void abort(Task task, Message error);
-
-    @Override
-    default void before(Task task, TaskManager.State state) {}
-
-    @Override
-    default void after(Task task, TaskManager.State state) {}
 }

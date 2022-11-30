5436aabcc1188cca36cc68e2912e9a77e4c392ef
Towhidul Islam <m.towhid.islam@gmail.com>
2022-11-30 18:39:24 +0600

    mem-cache:feat: implementing Lazy and Eger Mode

--- MemCache/src/main/java/com/infoworks/lab/microstream/MicroDataStore.java
+++ MemCache/src/main/java/com/infoworks/lab/microstream/MicroDataStore.java
@@ -2,6 +2,7 @@ package com.infoworks.lab.microstream;
 
 import com.it.soul.lab.data.base.DataStorage;
 import com.it.soul.lab.data.simple.SimpleDataSource;
+import one.microstream.reference.Lazy;
 import one.microstream.storage.embedded.types.EmbeddedStorage;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
 
@@ -15,22 +16,26 @@ import java.util.logging.Logger;
 public class MicroDataStore<Key, Value> extends SimpleDataSource<Key, Value> implements DataStorage, AutoCloseable {
 
     private static Logger LOG = Logger.getLogger(MicroDataStore.class.getSimpleName());
-    private final String uuid;
+    private final String location;
     private final EmbeddedStorageManager storage;
-    private Map<Key, Value> inMemStorage = new ConcurrentHashMap<>();
+    private RootObject<Key, Value> rootObject;
     private ExecutorService executors;
+    private final boolean enableLazyLoad;
 
-    public MicroDataStore(String location) {
-        this.uuid = location;
+    public MicroDataStore(String location, boolean enableLazyLoad) {
+        this.location = location;
+        this.enableLazyLoad = enableLazyLoad;
         this.storage   = EmbeddedStorage.start(Paths.get(location));
         boolean restored = retrieve();
         if (restored) LOG.info("Storage Restore Successful: @" + location);
         else LOG.info("Storage Initialization Successful: @" + location);
     }
 
+    public MicroDataStore(String location) { this(location, false); }
+
     @Override
     protected Map<Key, Value> getInMemoryStorage() {
-        return inMemStorage;
+        return rootObject.getMemStorage();
     }
 
     protected ExecutorService getExecutors() {
@@ -41,23 +46,28 @@ public class MicroDataStore<Key, Value> extends SimpleDataSource<Key, Value> imp
 
     @Override
     public String getUuid() {
-        return uuid;
+        return location;
+    }
+
+    public String getLocation() {
+        return location;
     }
 
     @Override
     public void save(boolean async) {
-        if(async) getExecutors().submit(() -> storage.store(inMemStorage));
-        else storage.store(inMemStorage);
+        if(async) getExecutors().submit(() -> storage.store(rootObject.getMemStorage()));
+        else storage.store(rootObject.getMemStorage());
     }
 
     @Override
     public boolean retrieve() {
         if (storage.root() == null) {
-            storage.setRoot(inMemStorage);
+            rootObject = enableLazyLoad ? new LazyRootObject(getLocation()) : new RootObject<>(getLocation());
+            storage.setRoot(rootObject);
             storage.storeRoot();
             return false;
         } else {
-            inMemStorage = (Map<Key, Value>) storage.root();
+            rootObject = (RootObject<Key, Value>) storage.root();
             return true;
         }
     }
@@ -76,4 +86,41 @@ public class MicroDataStore<Key, Value> extends SimpleDataSource<Key, Value> imp
             storage.shutdown();
         }
     }
+
+    ///////////////////////////////////////////Root-Object//////////////////////////////////////////////
+
+    private static class RootObject<Key, Value> {
+
+        private final String location;
+        private final Map<Key, Value> memStorage;
+
+        public RootObject(String location) {
+            this.location = location;
+            this.memStorage = new ConcurrentHashMap<>();
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public Map<Key, Value> getMemStorage() {
+            return memStorage;
+        }
+    }
+
+    private static class LazyRootObject<Key, Value> extends RootObject<Key, Value> {
+
+        private final Lazy<Map<Key, Value>> memStorage;
+
+        public LazyRootObject(String location) {
+            super(location);
+            this.memStorage = Lazy.Reference(new ConcurrentHashMap<>());
+        }
+
+        public Map<Key, Value> getMemStorage() {
+            return Lazy.get(memStorage);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 }

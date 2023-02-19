e42159bf6ba1e25505cc0764fc1d89d1b4ae9444
Towhidul Islam <m.towhid.islam@gmail.com>
2023-02-19 11:14:34 +0600

    rest:test: iResource test

--- HttpRestClient/src/test/java/com/infoworks/lab/util/services/iResourceServiceTest.java
+++ HttpRestClient/src/test/java/com/infoworks/lab/util/services/iResourceServiceTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
-
-import static org.junit.Assert.*;
 
 public class iResourceServiceTest {
 
@@ -36,17 +36,36 @@ public class iResourceServiceTest {
     }
 
     @Test
-    public void imageAsString() throws URISyntaxException, IOException {
+    public void imageAsString() throws IOException {
 
         iResourceService manager = iResourceService.create();
         File imfFile = new File("data/final-architecture.png");
         InputStream ios = manager.createStream(imfFile);
         //
         BufferedImage bufferedImage = manager.readAsImage(ios, BufferedImage.TYPE_INT_RGB);
+        ios.close();
         String base64Image = manager.readImageAsBase64(bufferedImage, iResourceService.Format.PNG);
         System.out.println("Message: " + base64Image);
         //
         BufferedImage decryptedImg = manager.readImageFromBase64(base64Image);
-        System.out.println("");
+        Assert.assertNotNull(decryptedImg);
+    }
+
+    @Test
+    public void imageAsStringWithAlternativeFileReading() throws IOException {
+
+        iResourceService manager = iResourceService.create();
+        Path path = Paths.get("src", "test", "resources");
+        File imfFile = new File(path.toFile().getAbsolutePath() + "/data/final-architecture.png");
+        InputStream ios = new FileInputStream(imfFile);
+        Assert.assertTrue(ios.available() > 0);
+        //
+        BufferedImage bufferedImage = manager.readAsImage(ios, BufferedImage.TYPE_INT_RGB);
+        ios.close();
+        String base64Image = manager.readImageAsBase64(bufferedImage, iResourceService.Format.PNG);
+        System.out.println("Message: " + base64Image);
+        //
+        BufferedImage decryptedImg = manager.readImageFromBase64(base64Image);
+        Assert.assertNotNull(decryptedImg);
     }
 }

13be8964b54632a98a339ce8698fc4692b8dce01
Islam <towhid.islam@degruyter.com>
2023-09-28 13:54:27 +0200

    vaadin:feat: adding PropertySearchBar ui component

---
+++ VaadinComponent/src/main/java/com/infoworks/lab/components/crud/components/views/search/PropertySearchBar.java
@@ -0,0 +1,125 @@
+package com.infoworks.lab.components.crud.components.views.search;
+
+import com.infoworks.lab.components.crud.Configurator;
+import com.infoworks.lab.components.crud.components.editor.AbstractBeanEditor;
+import com.infoworks.lab.components.crud.components.editor.BeanDialog;
+import com.it.soul.lab.sql.entity.EntityInterface;
+import com.it.soul.lab.sql.query.models.Property;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyModifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+
+public class PropertySearchBar<T extends EntityInterface> extends SearchBar<T> {
+
+    public static final String CLEAR_BUTTON_TITLE = "Clear";
+    public static final String SEARCH_BUTTON_TITLE = "Search";
+    private TextField searchField;
+    private Button newButton;
+    private Property searchProperty;
+    private Button searchButton;
+    ComboBox<Property> propKeyBox;
+
+    public PropertySearchBar(Class<T> beanType, SearchBarConfigurator configurator) {
+        super(beanType, configurator);
+    }
+
+    @Override
+    public Component prepareSearchView(SearchBarConfigurator configurator) {
+        HorizontalLayout layout = new HorizontalLayout();
+
+        propKeyBox = new ComboBox<>();
+        String[] skipProps = configurator.getSkipProperties();
+        propKeyBox.setItems(configurator.getProperties(getBeanType(), skipProps));
+        propKeyBox.setItemLabelGenerator(Property::getKey);
+        layout.add(propKeyBox);
+
+        searchField = new TextField();
+        searchField.setLabel("");
+        searchField.setPlaceholder("Search By Any...");
+        searchField.setPrefixComponent(new Icon("lumo", "search"));
+        searchField.setValueChangeMode(ValueChangeMode.EAGER);
+        searchField.addFocusShortcut(Key.KEY_F, KeyModifier.CONTROL);
+        layout.add(searchField);
+
+        searchButton = new Button(SEARCH_BUTTON_TITLE, new Icon("lumo", "search"));
+        searchButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        searchButton.addClickShortcut(Key.ENTER);
+        layout.add(searchButton);
+
+        if (!configurator.isHideAddNewButton()) {
+            newButton = new Button("Add New", new Icon("lumo", "plus"));
+            newButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+            newButton.addClickShortcut(Key.NEW);
+            layout.add(newButton);
+        }
+
+        return layout;
+    }
+
+    private void alterButton(Button button, String title, Icon icon) {
+        if (button.getText().equalsIgnoreCase(title))
+            return;
+        button.setText(title);
+        button.setIcon(icon);
+    }
+
+    @Override
+    public void configureDefaultEvents(Configurator configurator, BeanDialog dialog) {
+        //Action on AddNew Button on SearchBar:
+        if (!getConfigurator().isHideAddNewButton()) {
+            newButton.addClickListener(((event) -> {
+                try {
+                    EntityInterface ei = configurator.getBeanType().newInstance();
+                    dialog.open((T) ei, AbstractBeanEditor.Operation.ADD);
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }));
+        }
+        //Action setting up change event:
+        propKeyBox.addValueChangeListener((event) -> {
+            //System.out.println(event.getValue().getKey());
+            searchProperty = event.getValue();
+            alterButton(searchButton, SEARCH_BUTTON_TITLE, new Icon("lumo", "search"));
+        });
+        //Action on value-changed event on Search Field:
+        searchField.addValueChangeListener(((event) -> {
+            if (searchProperty != null) {
+                System.out.println("Search With: " + event.getValue());
+                searchProperty.setValue(event.getValue());
+            }
+            alterButton(searchButton, SEARCH_BUTTON_TITLE, new Icon("lumo", "search"));
+        }));
+        //Action on searchButton:
+        searchButton.addClickListener((event) -> {
+            if (event.getSource().getText().equalsIgnoreCase(CLEAR_BUTTON_TITLE)) {
+                //Clear the grid & related buttons:
+                searchField.clear();
+                propKeyBox.clear();
+                configurator.getDataSource().reloadGrid();
+            } else {
+                //Does not update grid when result comes from rest-api.
+                //configurator.getDataSource().addSearchFilter(event.getValue().toString());
+                if (searchProperty != null) {
+                    if (searchProperty.getValue() == null) {
+                        searchProperty.setValue(searchField.getValue());
+                    }
+                    configurator.getDataSource().addSearchFilters(configurator.getGridPageSize()
+                            , 0
+                            , searchProperty);
+                    alterButton(event.getSource(), CLEAR_BUTTON_TITLE, new Icon("lumo", "cross"));
+                }
+            }
+        });
+    }
+
+}
--- VaadinComponent/src/main/java/com/infoworks/lab/components/crud/components/views/search/SearchBarConfigurator.java
+++ VaadinComponent/src/main/java/com/infoworks/lab/components/crud/components/views/search/SearchBarConfigurator.java
@@ -1,7 +1,14 @@
 package com.infoworks.lab.components.crud.components.views.search;
 
+import com.it.soul.lab.sql.entity.EntityInterface;
+import com.it.soul.lab.sql.query.models.Property;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
 public class SearchBarConfigurator {
     private boolean hideAddNewButton;
+    String[] skipProperties = new String[0];
 
     public boolean isHideAddNewButton() {
         return hideAddNewButton;
@@ -9,6 +16,42 @@ public class SearchBarConfigurator {
 
     public SearchBarConfigurator setHideAddNewButton(boolean hideAddNewButton) {
         this.hideAddNewButton = hideAddNewButton;
+        return this;
+    }
+
+    public <T extends EntityInterface> List<Property> getProperties(Class<T> type
+            , String...skipKeys) {
+        T item = null;
+        try {
+            item = type.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        if (!Objects.nonNull(item)) return new ArrayList<>();
+        List<Property> properties = new ArrayList<>();
+        //
+        Map<String, Object> propMap = item.marshallingToMap(false);
+        propMap.forEach((key, value) -> {
+            if (Arrays.stream(skipKeys)
+                    .noneMatch(val -> val.equalsIgnoreCase(key))) {
+                properties.add(new Property(key, value));
+            }
+        });
+        return properties;
+    }
+
+    public String[] getSkipProperties() {
+        return skipProperties;
+    }
+
+    public SearchBarConfigurator setSkipProperties(String...skipProperties) {
+        this.skipProperties = skipProperties;
         return this;
     }
 }

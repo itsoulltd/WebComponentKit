61305274f994f4c20d5cac6d5da096c73a2ccb9e
Islam <towhid.islam@degruyter.com>
2023-11-08 17:53:27 +0100

    mem-cache:test: adding first usecase of testing

--- MemCache/src/test/java/com/infoworks/lab/microstream/InMemoryStatemachineTest.java
+++ MemCache/src/test/java/com/infoworks/lab/microstream/InMemoryStatemachineTest.java
@@ -1,13 +1,17 @@
 package com.infoworks.lab.microstream;
 
+import com.infoworks.lab.microstream.statemachine.Order;
 import com.infoworks.lab.microstream.statemachine.PizzaRecipe;
 import com.infoworks.lab.microstream.statemachine.PizzaService;
+import com.infoworks.lab.microstream.statemachine.orders.states.Confirmed;
+import com.infoworks.lab.microstream.statemachine.orders.states.Placed;
 import com.infoworks.lab.microstream.statemachine.pizzas.CheeseCrustPizza;
 import com.infoworks.lab.microstream.statemachine.pizzas.Pizza;
 import com.infoworks.lab.microstream.statemachine.pizzas.ThickCrustPizza;
 import com.infoworks.lab.microstream.statemachine.pizzas.ThinCrustPizza;
 import com.infoworks.lab.microstream.statemachine.toppings.*;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,21 +23,24 @@ public class InMemoryStatemachineTest {
     @Before
     public void before() {
         service = new PizzaService(location);
-        //Adding Recipes:
-        service.getRepository().save(new PizzaRecipe("1", "12.00", ThinCrustPizza.class.getName(), "Thin Crust Pizza"));
-        service.getRepository().save(new PizzaRecipe("2", "15.00", ThickCrustPizza.class.getName(), "Thick Crust Pizza"));
-        service.getRepository().save(new PizzaRecipe("3", "20.20", CheeseCrustPizza.class.getName(), "Cheese Crust Pizza"));
-        //
-        service.getRepository().save(new PizzaRecipe("4", "1.00", Cheese.class.getName(), "Cheese"));
-        service.getRepository().save(new PizzaRecipe("5", "2.00", Mushroom.class.getName(), "Mushroom"));
-        service.getRepository().save(new PizzaRecipe("6", "1.50", Pepperoni.class.getName(), "Pepperoni"));
-        service.getRepository().save(new PizzaRecipe("7", "2.70", Sausage.class.getName(), "Sausage"));
-        //
+        //If no seed data already added:
+        if (service.getRepository().count() <= 0) {
+            //Adding Recipes:
+            service.getRepository().save(new PizzaRecipe("1", "12.00", ThinCrustPizza.class.getName(), "Thin Crust Pizza"));
+            service.getRepository().save(new PizzaRecipe("2", "15.00", ThickCrustPizza.class.getName(), "Thick Crust Pizza"));
+            service.getRepository().save(new PizzaRecipe("3", "20.20", CheeseCrustPizza.class.getName(), "Cheese Crust Pizza"));
+            //
+            service.getRepository().save(new PizzaRecipe("4", "1.00", Cheese.class.getName(), "Cheese"));
+            service.getRepository().save(new PizzaRecipe("5", "2.00", Mushroom.class.getName(), "Mushroom"));
+            service.getRepository().save(new PizzaRecipe("6", "1.50", Pepperoni.class.getName(), "Pepperoni"));
+            service.getRepository().save(new PizzaRecipe("7", "2.70", Sausage.class.getName(), "Sausage"));
+            //
+        }
     }
 
     @After
     public void after() throws Exception {
-        service.getRepository().clear();
+        //service.getRepository().clear();
         service.close();
     }
 
@@ -43,6 +50,21 @@ public class InMemoryStatemachineTest {
         //SimpleDataSource<String, Pizza> pizzas = new MicroDataStore<>(location + "/pizza");
         //SimpleDataSource<String, StateMachine> machines = new MicroDataStore<>(location + "/statemachine");
         //TODO
+        Order newOrder = service.createOrder(whichCrust("thick"));
+        newOrder.setCustomerEmail("m.towhid@gmail.com");
+        service.addToppings(newOrder, null);
+        //
+        //Assert: current price
+        Assert.assertEquals("15.00", newOrder.getPrice());
+        System.out.println("Price: " + newOrder.getPrice());
+        //Assert: state
+        Assert.assertEquals(Placed.class.getName(), newOrder.getStateClassName());
+        System.out.println("State: " + newOrder.getStateClassName());
+
+        service.changeState(newOrder);
+
+        Assert.assertEquals(Confirmed.class.getName(), newOrder.getStateClassName());
+        System.out.println("State: " + newOrder.getStateClassName());
     }
 
     private Class<? extends Pizza> whichCrust(String crustName) {

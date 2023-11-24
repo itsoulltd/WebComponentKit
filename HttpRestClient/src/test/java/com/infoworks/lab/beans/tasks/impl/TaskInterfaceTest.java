36f889e445b572399067a038c8f876ed61b86314
Towhid Islam <m.towhid.islam@gmail.com>
2023-11-24 08:15:57 +0100

    http-client:test: refactoring

--- HttpRestClient/src/test/java/com/infoworks/lab/beans/tasks/impl/TaskInterfaceTest.java
+++ HttpRestClient/src/test/java/com/infoworks/lab/beans/tasks/impl/TaskInterfaceTest.java
@@ -11,6 +11,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TaskInterfaceTest {
 
     private TaskStack stack;
@@ -30,12 +33,47 @@ public class TaskInterfaceTest {
 
     @Test
     public void taskSubclassTestInStack() {
+        //Initialize:
+        CountDownLatch latch = new CountDownLatch(1);
         //
+        stack.push(new BasicTask());
+        stack.push(new BasicExecutableTask());
+        stack.push(new TaskWithCustomConstructor("James", 29));
+        stack.push(new ExeTaskWithCustomConstructor("Sohana", 23));
+        stack.commit(true, (message, state) -> {
+            System.out.println("State: " + state.name());
+            System.out.println(message.toString());
+            latch.countDown();
+        });
+        //
+        try {
+            latch.await();
+        } catch (InterruptedException e) {}
     }
 
     @Test
     public void taskSubclassTestInQueue() {
+        //Initialize:
+        CountDownLatch latch = new CountDownLatch(1);
+        AtomicInteger counter = new AtomicInteger(4);
         //
+        queue.onTaskComplete((message, state) -> {
+            System.out.println("State: " + state.name());
+            System.out.println(message.toString());
+            if (counter.get() > 1) {
+                counter.decrementAndGet();
+            } else {
+                latch.countDown();
+            }
+        });
+        queue.add(new BasicTask());
+        queue.add(new BasicExecutableTask());
+        queue.add(new TaskWithCustomConstructor("James", 29));
+        queue.add(new ExeTaskWithCustomConstructor("Sohana", 23));
+        //
+        try {
+            latch.await();
+        } catch (InterruptedException e) {}
     }
 
     ////////////////////////////////////////////////////////////////////////////////
@@ -44,11 +82,13 @@ public class TaskInterfaceTest {
 
         @Override
         public Response execute(Message message) throws RuntimeException {
+            System.out.println(String.format("%s: %s", BasicTask.class.getSimpleName(), "Success!"));
             return new Response().setStatus(200).setMessage("Success!");
         }
 
         @Override
         public Response abort(Message message) throws RuntimeException {
+            System.out.println(String.format("%s: %s", BasicTask.class.getSimpleName(), "Error!"));
             return new Response().setStatus(500).setMessage("Error!");
         }
     }
@@ -57,6 +97,7 @@ public class TaskInterfaceTest {
 
         @Override
         public Response execute(Message message) throws RuntimeException {
+            System.out.println(String.format("%s: %s", BasicExecutableTask.class.getSimpleName(), "Success!"));
             return new Response().setStatus(200).setMessage("Success!");
         }
     }
@@ -71,11 +112,14 @@ public class TaskInterfaceTest {
         public Response execute(Message message) throws RuntimeException {
             String name = getPropertyValue("name").toString();
             int age = Integer.valueOf(getPropertyValue("age").toString());
+            System.out.println(String.format("%s: Success! %s, %s"
+                    , TaskWithCustomConstructor.class.getSimpleName(), name, age));
             return new Response().setStatus(200).setMessage(String.format("Success! %s, %s", name, age));
         }
 
         @Override
         public Response abort(Message message) throws RuntimeException {
+            System.out.println(String.format("%s: %s", TaskWithCustomConstructor.class.getSimpleName(), "Error!"));
             return new Response().setStatus(500).setMessage("Error!");
         }
     }
@@ -90,11 +134,11 @@ public class TaskInterfaceTest {
         public Response execute(Message message) throws RuntimeException {
             String name = getPropertyValue("name").toString();
             int age = Integer.valueOf(getPropertyValue("age").toString());
+            System.out.println(String.format("%s: Success! %s, %s"
+                    , ExeTaskWithCustomConstructor.class.getSimpleName(), name, age));
             return new Response().setStatus(200).setMessage(String.format("Success! %s, %s", name, age));
         }
     }
-
-    ////////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////////
 

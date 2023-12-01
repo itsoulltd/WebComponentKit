abf3e0b9b3204a3fea0a121f3b1b2c22d7a46f44
Towhid Islam <m.towhid.islam@gmail.com>
2023-12-01 21:53:36 +0100

    rest-client:test: adding example FetchRequest, ItemCountRequest, SearchRequest

---
+++ HttpRestClient/src/test/java/com/infoworks/lab/beans/task/FetchRequest.java
@@ -0,0 +1,46 @@
+package com.infoworks.lab.beans.task;
+
+import com.infoworks.lab.beans.tasks.nuts.ExecutableTask;
+import com.infoworks.lab.rest.models.Message;
+import com.infoworks.lab.rest.models.Response;
+import com.infoworks.lab.rest.repository.RestRepository;
+
+import java.util.List;
+
+public class FetchRequest<T> extends ExecutableTask<Message, Response> {
+
+    private RestRepository repository;
+    private int page;
+    private int pageSize;
+    private final int delay;
+
+    public FetchRequest(RestRepository repository, int page, int pageSize) {
+        this(repository, page, pageSize, 1000); //default 1 sec delay
+    }
+
+    public FetchRequest(RestRepository repository, int page, int pageSize, int delay) {
+        this.repository = repository;
+        this.page = page;
+        this.pageSize = pageSize;
+        this.delay = delay;
+    }
+
+    @Override
+    public Response execute(Message message) throws RuntimeException {
+        if (delay > 0) {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {}
+        }
+        //
+        try {
+            List<T> res = repository.fetch(page, pageSize);
+            System.out.println("Items count: " + res.size());
+            String json = Message.marshal(res);
+            return new Response().setStatus(200).setMessage(json);
+        } catch (Exception e) {
+            return new Response().setStatus(500).setError(e.getMessage());
+        }
+    }
+
+}
---
+++ HttpRestClient/src/test/java/com/infoworks/lab/beans/task/ItemCountRequest.java
@@ -0,0 +1,26 @@
+package com.infoworks.lab.beans.task;
+
+import com.infoworks.lab.beans.tasks.nuts.ExecutableTask;
+import com.infoworks.lab.rest.models.ItemCount;
+import com.infoworks.lab.rest.models.Message;
+import com.infoworks.lab.rest.models.Response;
+import com.infoworks.lab.rest.repository.RestRepository;
+
+public class ItemCountRequest extends ExecutableTask<Message, Response> {
+
+    private RestRepository repository;
+
+    public ItemCountRequest(RestRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Response execute(Message message) throws RuntimeException {
+        try {
+            ItemCount count = repository.rowCount();
+            return new Response().setStatus(200).setMessage(count.toString());
+        } catch (Exception e) {
+            return new Response().setStatus(500).setError(e.getMessage());
+        }
+    }
+}

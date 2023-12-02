43ebdd42af863ff6c4a22f164826cc24bb5c510d
Towhid Islam <m.towhid.islam@gmail.com>
2023-12-02 14:51:05 +0100

    rest-client:test: adding Rest(Get/Post/Put/Delete)Request

---
+++ HttpRestClient/src/test/java/com/infoworks/lab/beans/task/rest/GetRequest.java
@@ -0,0 +1,27 @@
+package com.infoworks.lab.beans.task.rest;
+
+import com.infoworks.lab.beans.tasks.nuts.AbstractTask;
+import com.infoworks.lab.rest.models.Message;
+import com.infoworks.lab.rest.models.Response;
+import com.infoworks.lab.rest.template.HttpInteractor;
+
+public class GetRequest<In extends Message, Out extends Response> extends AbstractTask<In, Out> {
+
+    private HttpInteractor<Out,In> template;
+
+    public void setTemplate(HttpInteractor<Out, In> template) {
+        this.template = template;
+    }
+
+    public GetRequest() {}
+
+    @Override
+    public Out execute(In message) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public Out abort(In message) throws RuntimeException {
+        return null;
+    }
+}

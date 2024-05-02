3a75202492c8562090933eadc23d35ff9c656da9
Towhid Islam <m.towhid.islam@gmail.com>
2024-05-02 23:34:43 +0200

    re

--- HttpRestClient/src/test/java/com/infoworks/lab/rest/models/MessageTest.java
+++ HttpRestClient/src/test/java/com/infoworks/lab/rest/models/MessageTest.java
@@ -1,17 +1,19 @@
 package com.infoworks.lab.rest.models;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.infoworks.lab.rest.models.events.Event;
 import com.infoworks.lab.rest.models.events.EventType;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.Objects;
-import java.util.UUID;
-
-import static org.junit.Assert.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class MessageTest {
+
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Test
     public void payloadTest(){
@@ -31,7 +33,37 @@ public class MessageTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void mapConversionWithDefault() throws IOException, ParseException {
+        Map<String, Object> data = new HashMap<>();
+        data.put("X-Auth-Token", "token-by-token");
+        data.put("Status", 200);
+        data.put("Datetime", format.parse("2021-12-31 23:01:00"));
+        String json = Message.marshal(data);
+        System.out.println(json);
+        //
+        Map<String, Object> reconstructed = Message.unmarshal(Map.class, json);
+        Assert.assertEquals(reconstructed.get("X-Auth-Token"), "token-by-token");
+        Assert.assertEquals(reconstructed.get("Status"), 200);
+        Assert.assertEquals(new Date(Long.parseLong(reconstructed.get("Datetime").toString()))
+                , format.parse("2021-12-31 23:01:00"));
+    }
+
+    @Test
+    public void mapConversionWithTypeReference() throws IOException, ParseException {
+        Map<String, Object> data = new HashMap<>();
+        data.put("X-Auth-Token", "token-by-token");
+        data.put("Status", 200);
+        data.put("Datetime", format.parse("2021-12-31 23:01:00"));
+        String json = Message.marshal(data);
+        System.out.println(json);
+        //
+        Map<String, String> reconstructed = Message.unmarshal(new TypeReference<Map<String, String>>() {}, json);
+        Assert.assertEquals(reconstructed.get("X-Auth-Token"), "token-by-token");
+        Assert.assertEquals(reconstructed.get("Status"), "200");
+        Assert.assertEquals(reconstructed.get("Datetime"), "1640988060000");
     }
 
 }

dfbde17921b70d88d9319450db2a3ec50e1855a6
Towhid Islam <m.towhid.islam@gmail.com>
2025-05-14 09:40:42 +0200

    rest-client:fix: DeleteTask InputStream need to close after use. Tested

--- HttpRestClient/src/test/java/com/infoworks/lab/beans/tasks/impl/RestTaskTest.java
+++ HttpRestClient/src/test/java/com/infoworks/lab/beans/tasks/impl/RestTaskTest.java
@@ -7,12 +7,21 @@ import com.infoworks.lab.rest.models.Response;
 import com.infoworks.lab.rest.models.SearchQuery;
 import com.infoworks.lab.rest.models.pagination.Pagination;
 import com.infoworks.lab.rest.models.pagination.SortOrder;
+import com.infoworks.lab.util.services.iResourceService;
 import com.it.soul.lab.sql.query.models.Row;
 import org.junit.Test;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 public class RestTaskTest {
 
@@ -196,6 +205,108 @@ public class RestTaskTest {
         try {
             latch.await();
         } catch (InterruptedException e) {}
+    }
+
+    @Test
+    public void downloadTaskTest() {
+        //TaskFlow:
+        //Test Url-1: https://farm7.staticflickr.com/6089/6115759179_86316c08ff_z_d.jpg
+        //
+        DownloadTask task = new DownloadTask("https://farm7.staticflickr.com/6089/6115759179_86316c08ff_z_d.jpg"
+                , null);
+        task.setToken("my-token");
+        DownloadTask.ResourceResponse response = task.execute(null);
+        System.out.println("Status: " + response.getStatus());
+        //
+        if (response.getResource() != null) {
+            try (InputStream iso = response.getResource().getInputStream()) {
+                iResourceService service = iResourceService.create();
+                BufferedImage img = service.readAsImage(iso, TYPE_INT_RGB);
+                System.out.println("Image Downloaded: " + response.getResource().getFilename());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void downloadTaskTest2() {
+        //TaskFlow:
+        //Test Url-2: https://farm2.staticflickr.com/1090/4595137268_0e3f2b9aa7_z_d.jpg
+        //
+        DownloadTask task = new DownloadTask("https://farm7.staticflickr.com/6089/6115759179_86316c08ff_z_d.jpg"
+                , null);
+        task.setToken("my-token");
+        DownloadTask.ResourceResponse response = task.execute(null);
+        System.out.println("Status: " + response.getStatus());
+        //
+        if (response.getResource() != null) {
+            try (InputStream iso = response.getResource().getInputStream()) {
+                iResourceService service = iResourceService.create();
+                BufferedImage img = service.readAsImage(iso, TYPE_INT_RGB);
+                System.out.println("Image Downloaded: " + response.getResource().getFilename());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void downloadTaskTest3() {
+        //TaskFlow:
+        //Test Url-2: https://farm2.staticflickr.com/1090/4595137268_0e3f2b9aa7_z_d.jpg
+        //
+        DownloadTask task = new DownloadTask("https://farm2.staticflickr.com/1090/4595137268_0e3f2b9aa7_z_d.jpg"
+                , null);
+        task.setToken("my-token");
+        task.addResponseListener((encoded) -> {
+            System.out.println(encoded != null ? encoded.length() : "0");
+            try {
+                String decoded = new String(Base64.getDecoder().decode(encoded), "UTF-8");
+                System.out.println(decoded);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        });
+        DownloadTask.ResourceResponse response = task.execute(null);
+        System.out.println("Status: " + response.getStatus());
+        //
+        if (response.getResource() != null) {
+            System.out.println("Image Downloaded: " + response.getResource().getFilename());
+        }
+    }
+
+    @Test
+    public void downloadTaskTest4() {
+        //TaskFlow:
+        //Test Url-4: https://farm2.staticflickr.com/1090/bb3_z_d.jpg
+        //
+        DownloadTask task = new DownloadTask("https://farm2.staticflickr.com/1090/bb3_z_d.jpg"
+                , null);
+        task.setToken("my-token");
+        task.addResponseListener((encoded) -> {
+            System.out.println(encoded != null ? encoded.length() : "0");
+        });
+        DownloadTask.ResourceResponse response = task.execute(null);
+        System.out.println("Status: " + response.getStatus());
+        System.out.println("Error: " + response.getError());
+        //
+    }
+
+    @Test
+    public void downloadTaskTest5() {
+        //TaskFlow:
+        //Test Url-5: "" or Null
+        //
+        DownloadTask task = new DownloadTask(null, null);
+        task.setToken("my-token");
+        task.addResponseListener((encoded) -> {
+            System.out.println(encoded != null ? encoded.length() : "0");
+        });
+        DownloadTask.ResourceResponse response = task.execute(null);
+        System.out.println("Status: " + response.getStatus());
+        System.out.println("Error: " + response.getError());
+        //
     }
 
 }

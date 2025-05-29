01c436fbf74f9e7653ac76d38168f1aa32aabbf7
Towhid Islam <m.towhid.islam@gmail.com>
2025-05-29 16:37:00 +0200

    ledger:refactor: updated

--- LedgerBook/src/test/java/com/itsoul/lab/application/bank/LedgerBook.java
+++ LedgerBook/src/test/java/com/itsoul/lab/application/bank/LedgerBook.java
@@ -53,7 +53,7 @@ public class LedgerBook {
         Money money = null;
         Ledger book = null;
         try {
-            book = getLedger(chartOfAccounts);
+            book = initLedger(chartOfAccounts);
             money = Money.toMoney(deposit, currency);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
@@ -79,7 +79,7 @@ public class LedgerBook {
         Money money = null;
         Ledger book = null;
         try {
-            book = getLedger(chartOfAccounts);
+            book = initLedger(chartOfAccounts);
             money = book.getAccountBalance(cash_account);
         } catch (Exception e) {
             money = Money.NULL_MONEY;
@@ -99,7 +99,7 @@ public class LedgerBook {
         Ledger book = null;
         boolean isExist = false;
         try{
-            book = getLedger(chartOfAccounts);
+            book = initLedger(chartOfAccounts);
             isExist = book.getAccountBalance(cash_account) != null;
         }catch (Exception e) {} finally {
             if (book != null)
@@ -131,7 +131,7 @@ public class LedgerBook {
                     .retrive(to)
                     .build();
             //
-            book = getLedger(chartOfAccounts);
+            book = initLedger(chartOfAccounts);
 
             //Transfer request:
             TransferRequest transferRequest1 = book.createTransferRequest()
@@ -153,7 +153,7 @@ public class LedgerBook {
         }
     }
 
-    private Ledger getLedger(ChartOfAccounts chartOfAccounts){
+    private Ledger initLedger(ChartOfAccounts chartOfAccounts){
         Ledger book = new Ledger.LedgerBuilder(chartOfAccounts)
                 .name("General Ledger")
                 .connector(getConnector())
@@ -210,7 +210,7 @@ public class LedgerBook {
         ChartOfAccounts chartOfAccounts = new ChartOfAccounts.ChartOfAccountsBuilder()
                 .retrive(cash_account)
                 .build();
-        Ledger book = getLedger(chartOfAccounts);
+        Ledger book = initLedger(chartOfAccounts);
         List<Transaction> cashAccountTransactionList = book.findTransactions(cash_account);
         return cashAccountTransactionList;
     }

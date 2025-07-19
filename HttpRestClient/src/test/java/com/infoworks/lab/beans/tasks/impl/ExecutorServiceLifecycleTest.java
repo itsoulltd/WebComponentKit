f63add25adf24044662c90670b202e63299d9086
Towhid Islam <m.towhid.islam@gmail.com>
2025-07-19 17:20:44 +0200

    rest-client:test: ExecutorService lifecycle example.

--- HttpRestClient/src/test/java/com/infoworks/lab/beans/tasks/impl/ExecutorServiceLifecycleTest.java
+++ HttpRestClient/src/test/java/com/infoworks/lab/beans/tasks/impl/ExecutorServiceLifecycleTest.java
@@ -2,35 +2,76 @@ package com.infoworks.lab.beans.tasks.impl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class ExecutorServiceLifecycleTest {
 
     public static void main(String[] args) {
         int jobCount = 5;
         ExecutorService service = Executors.newFixedThreadPool(2);
-        List<Future<String>> handlers = new ArrayList<>();
+        List<Future<String>> futureMessages = new ArrayList<>();
 
+        //1. Submit all tasks:
         for (int i = 0; i < jobCount; i++) {
-            Future<String> handle = service.submit(new MyTask());
-            handlers.add(handle);
+            Future<String> future = service.submit(new MyTask());
+            futureMessages.add(future);
         }
 
-        //When call shutdown() on service:
-        //1. returns immediately
-        //2. cause subsequent submit or execute request to be rejected
-        //3. pool moves to shutdown state
-        //4. when (if) all the currently submitted task complete, pool moves to terminate state.
+        //6. Cancel a job, that's in mid-run:
+        /*try {
+            Thread.sleep(800);
+            Future<String> secondFuture = futureMessages.get(1);
+            secondFuture.cancel(true);
+        } catch (InterruptedException e) {
+            System.out.println("main thread interrupted?");
+        }*/
+
+        //2. When call shutdown() on service:
+        //a. returns immediately
+        //b. cause subsequent submit or execute request to be rejected
+        //c. pool moves to shutdown state
+        //d. when (if) all the currently submitted task complete, pool moves to terminate state.
         service.shutdown();
-        //What if we call shutdownNow()
+
+        //3. What if we call shutdownNow()
         //service.shutdownNow();
 
         //Note: If we do not call service.shutdown(), it means the non-daemon threads in pool are alive,
         //So the main thread shall be alive, unless we force quite.
+
+        //5. Receives the results:
+        System.out.println("All jobs submitted to the pool:");
+        while (futureMessages.size() > 0) {
+            Iterator<Future<String>> iterator = futureMessages.iterator();
+            while (iterator.hasNext()) {
+                Future<String> now = iterator.next();
+                if (now.isDone()) {
+                    iterator.remove();
+                    try {
+                        String result = now.get();
+                        System.out.println("Got a job result: " + result);
+                    } catch (InterruptedException e) {
+                        //should never happened:
+                        System.out.println("main thread interrupted??");
+                    } catch (ExecutionException e) {
+                        System.out.println("Job throw an exception: " + e.getCause());
+                    } catch (CancellationException e) {
+                        System.out.println("Job was canceled!");
+                    }
+                }
+            }//
+        }
+        //End of While-01
+
+        //4. Await until termination:
+        try {
+            service.awaitTermination(10, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            System.out.println("main thread interrupted???");
+        }
+        System.out.println("main exiting!");
     }
 
 }
